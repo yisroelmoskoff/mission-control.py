f1d5268286b38074ac98770eef623bdc9c2b2e88
yisroel <yisroelmoskoff@gmail.com>
2026-06-18 18:59:00 +0300

    fuel_balance

--- mission.py
+++ mission.py
@@ -1,19 +1,22 @@
-# name = "yisrael"
-# mission_code = 153
-# distance_to_target = 0.950
-# is_mission_active = False
-# # print(name ,mission_code,distance_to_target,is_mission_active)
-# print(type(name))
-# print(type(mission_code))
-# print(type(is_mission_active))
-# print(type(distance_to_target))
+name = "yisrael"
+mission_code = 153
+distance_to_target = 0.950
+is_mission_active = False
+# print(name ,mission_code,distance_to_target,is_mission_active)
+print(type(name))
+print(type(mission_code))
+print(type(is_mission_active))
+print(type(distance_to_target))
 
-#  base_target_distance = 950*2
-# full_base_target_distance=1900 
-# print(full_base_target_distance,base_target_distance)
+base_target_distance = 950 * 2
+full_base_target_distance=1900 
+print(full_base_target_distance,base_target_distance)
 
 fuel_usage = 1/15
 trip_fuel_amount = 1900/15
 print(fuel_usage,trip_fuel_amount)
 
+total_fuel = 200
+fuel_balance = 200-126.667
+print(total_fuel,fuel_balance)
 
