70bfb19b15fd58021c48fd976ddb12c8ed208132
yisroel <yisroelmoskoff@gmail.com>
2026-06-18 15:13:36 +0300

    the_type_of_mission_data

--- mission.py
+++ mission.py
@@ -2,4 +2,8 @@ name = "yisrael"
 mission_code = 153
 distance_to_target = 0.950
 is_mission_active = False
-print(name ,mission_code,distance_to_target,is_mission_active)
+# print(name ,mission_code,distance_to_target,is_mission_active)
+print(type(name))
+print(type(mission_code))
+print(type(is_mission_active))
+print(type(distance_to_target))
