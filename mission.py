a1ff78a4f57df90f6683a98c91c0a6660ba75bcf
yisroel <yisroelmoskoff@gmail.com>
2026-06-18 15:58:23 +0300

    distance_to_targrt

--- mission.py
+++ mission.py
@@ -1,9 +1,14 @@
-name = "yisrael"
-mission_code = 153
-distance_to_target = 0.950
-is_mission_active = False
-# print(name ,mission_code,distance_to_target,is_mission_active)
-print(type(name))
-print(type(mission_code))
-print(type(is_mission_active))
-print(type(distance_to_target))
+# name = "yisrael"
+# mission_code = 153
+# distance_to_target = 0.950
+# is_mission_active = False
+# # print(name ,mission_code,distance_to_target,is_mission_active)
+# print(type(name))
+# print(type(mission_code))
+# print(type(is_mission_active))
+# print(type(distance_to_target))
+
+ base_target_distance = 950*2
+full_base_target_distance=1900 
+print(full_base_target_distance,base_target_distance)
+
