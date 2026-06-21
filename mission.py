c2b3b0ebf5b510f51238b50be3bb84c38b95f51e
yisroel <yisroelmoskoff@gmail.com>
2026-06-21 13:37:23 +0300

    added_fuelin_milles_and_agent_name

--- mission.py
+++ mission.py
@@ -1,23 +1,35 @@
+# mission data
 name = "yisrael"
 mission_code = 153
 distance_to_target = 0.950
 is_mission_active = False
-# print(name ,mission_code,distance_to_target,is_mission_active)
+print(name ,mission_code,distance_to_target,is_mission_active)
 print(type(name))
 print(type(mission_code))
 print(type(is_mission_active))
 print(type(distance_to_target))
 
+# travel distance 
 base_target_distance = 950 * 2
 full_base_target_distance=1900 
 print(full_base_target_distance,base_target_distance)
 
+# fuel estimation
 fuel_usage = 1/15
 trip_fuel_amount = 1900/15
 print(fuel_usage,trip_fuel_amount)
 
+# remaning fuel
 total_fuel = 200
 fuel_balance = 200-126.667
 print(total_fuel,fuel_balance)
 
-time_of_mission = (input(how long did  the mission take using hours,minuts,second))
+time_of_mission = (input('how long did  the mission take using hours,minuts,second'))
+print(time_of_mission)
+
+
+distance_in_miles = (int(input("how much km"))*0.62)
+print(distance_in_miles)
+
+new_agent_name = (input("what is your name"))
+print(new_agent_name,name)
